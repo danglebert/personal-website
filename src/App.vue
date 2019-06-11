6afae8e882e07b86de3f3e4d72a853942de63dd9
Dan Gilbert <daniel.gilbert0051@gmail.com>
2019-06-11 15:42:04 -0400

    feat: fade in animation

--- src/App.vue
+++ src/App.vue
@@ -24,9 +24,6 @@ body {
   height: 100%;
   margin: 0;
 }
-#nav {
-  height: 10%;
-}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -34,15 +31,5 @@ body {
   text-align: center;
   color: #2c3e50;
   height: 100%;
-}
-.home {
-  background-image: url('./assets/background.jpg');
-  background-repeat: no-repeat;
-  background-size: 100% 100%;
-  height: 100%;
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
 }
 </style>
--- src/components/Heading.vue
+++ src/components/Heading.vue
@@ -1,15 +1,41 @@
 <template>
   <div class="heading">
-    <h1>Dan Gilbert</h1>
-    <h3>Full Stack Software Engineer</h3>
+    <transition name="fade" v-on:enter="nameEnter" v-on:before-enter="beforeEnter">
+      <h1 v-if="show">Dan Gilbert</h1>
+    </transition>
+    <transition v-on:enter="descEnter" v-on:before-enter="beforeEnter">
+      <h3 v-if="show">Full Stack Software Engineer</h3>
+    </transition>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      show: false
+    };
+  },
+  methods: {
+    beforeEnter: function(el) {
+      el.style.opacity = 0;
+    },
+    nameEnter(el, done) {
+      Velocity(el, { opacity: 1 }, { duration: 1500 });
+      done();
+    },
+    descEnter(el, done) {
+      setTimeout(() => {
+        Velocity(el, { opacity: 1 }, { duration: 1500 });
+      }, 1500);
+      done();
+    }
+  },
+  created() {
+    setTimeout(() => {
+      this.show = true;
+    }, 500);
   }
 };
 </script>
@@ -28,11 +54,20 @@ h3 {
   text-shadow: 1px 1px white;
 }
 
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+
 .heading {
   background-image: url('../assets/background.jpg');
   background-repeat: no-repeat;
   background-size: 100% 100%;
-  height: 100%;
+  background-attachment: fixed;
+  height: 100vh;
   width: 100%;
   display: flex;
   flex-direction: column;
