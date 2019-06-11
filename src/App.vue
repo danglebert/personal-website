568d9b5019e1ccced9a54f23b62f0967e333cd11
Dan Gilbert <daniel.gilbert0051@gmail.com>
2019-06-11 14:20:17 -0400

    task: est home image/heading, remove router

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,48 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Heading/>
   </div>
 </template>
 
+<script>
+// @ is an alias to /src
+import Heading from '@/components/Heading.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Heading
+  }
+};
+</script>
+
 <style>
+html {
+  height: 100%;
+}
+body {
+  height: 100%;
+  margin: 0;
+}
+#nav {
+  height: 10%;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+}
+.home {
+  background-image: url('./assets/background.jpg');
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
---
+++ src/components/Heading.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="heading">
+    <h1>Dan Gilbert</h1>
+    <h3>Full Stack Software Engineer</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  margin: 0;
+  color: white;
+  font-size: 4em;
+  text-shadow: 2px 2px #ff0000;
+}
+h3 {
+  margin: 0;
+  font-size: 2em;
+  text-shadow: 1px 1px white;
+}
+
+.heading {
+  background-image: url('../assets/background.jpg');
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
